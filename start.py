e0b73c943dd801f37c590c916ff789879ad6eb4e
mili-git <61312666+mili-git@users.noreply.github.com>
2020-03-22 12:22:31 +0100

    Verlinkung Navbar

--- start.py
+++ start.py
@@ -2,9 +2,13 @@ from flask import Flask
 from flask import render_template
 app = Flask("Name App")
 
-@app.route('/hello')
-def hello_world():
-    return render_template("test.html")
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/rm-calculator')
+def rm_calculator():
+    return render_template("1RM.html")    
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
