21037380f133680df295516c5cb2292ac678e6e4
mili-git <61312666+mili-git@users.noreply.github.com>
2020-03-25 22:20:34 +0100

    Navigation ergänzt

--- start.py
+++ start.py
@@ -10,5 +10,9 @@ def index():
 def rm_calculator():
     return render_template("1RM.html")    
 
+@app.route('/Tr4ining')
+def training():
+    return render_template("training.html")    
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
