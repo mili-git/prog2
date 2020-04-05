57a1890a0048beeeb95ca91c11f9c7396048d525
mili-git <61312666+mili-git@users.noreply.github.com>
2020-04-05 13:56:27 +0200

    Trainigsseite umgesetzt

--- start.py
+++ start.py
@@ -3,11 +3,12 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask import url_for
-import json 
+import json
 
 app = Flask("Name App")
 
 rm_json_pfad = "./rm.json"
+trainingseinheiten_json_pfad = "./traingseinheiten.json"
 
 def lade_daten_aus_json (pfad, standard_wert = []):
 	#https://www.programiz.com/python-programming/json
@@ -22,7 +23,10 @@ def schreibe_daten_in_json(pfad, daten):
 	with open(pfad, 'w') as datei:
 		json.dump(daten, datei)
 
-
+def berechne_rm(gewicht, wiederholungen):
+	#https://en.wikipedia.org/wiki/One-repetition_maximum#Lombardi 
+	resultat = gewicht*wiederholungen**0.10 
+	return resultat
 
 @app.route('/')
 def index():
@@ -45,20 +49,50 @@ def rm_calculator():
 
 		if rm in alle_rms:
 			return render_template("1rm.html", existiert_bereits = True)
-
+		#Berechne 1 RM
+		ergebnis = berechne_rm(gewicht, wiederholungen)
+		rm["resultat"]= ergebnis
 		alle_rms.append(rm)
+
 
 		schreibe_daten_in_json(rm_json_pfad, alle_rms)
 		return redirect(url_for("training"))
+	return render_template("1rm.html", existiert_bereits = False)    
 
-    return render_template("1rm.html", existiert_bereits = False)    
-
-@app.route('/training')
+@app.route('/training', methods=["GET", "POST"])
 def training():
-    return render_template("training.html")    
+	rms = lade_daten_aus_json(rm_json_pfad, [])
+	if request.method == "POST":
+		trainingseinheiten = lade_daten_aus_json(trainingseinheiten_json_pfad, [])
+
+
+		#Extrahiere info aus dem request
+		rm = request.form["rm"]
+		verbesserungsbereich = request.form["verbesserungsbereich"]
+		anzahltrainingseinheiten = int(request.form["trainingseinheiten"])
+		startdatum = request.form["startdatum"]
+		enddatum = request.form["enddatum"]
+
+		training = {
+			"rm": rm,
+			"verbesserungsbereich": verbesserungsbereich,
+			"anzahltrainingseinheiten": anzahltrainingseinheiten,
+			"startdatum": startdatum,
+			"enddatum": enddatum
+		}
+		
+		if training in trainingseinheiten:
+			return render_template("training.html", existiert_bereits = True)
+		trainingseinheiten.append(training)
+		schreibe_daten_in_json(trainingseinheiten_json_pfad, trainingseinheiten)
+		return redirect(url_for("fortschritt"))
+
+
+		
+	return render_template("training.html", rms = rms)  
 
 @app.route('/fortschritt')
-def fort_schritt():
+def fortschritt():
     return render_template("fortschritt.html")  
 
 @app.route('/resultate')
